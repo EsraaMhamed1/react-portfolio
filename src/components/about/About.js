1bf37c5786d665dac044e1c4bee70d068ae31cc9
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2024-11-30 20:55:57 +0200

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -48,8 +48,17 @@ const About = () => {
 							<p>Next JS </p>
 							<hr style={{ width: '65%' }} />
 						</div>
+	<div className='about-skill'>
+							<p>Tailwind CSS</p> <hr style={{ width: '95%' }} />
+						</div>
 						<div className='about-skill'>
 							<p>Materila Ui</p> <hr style={{ width: '75%' }} />
+						</div>
+	<div className='about-skill'>
+							<p>R Studio</p> <hr style={{ width: '65%' }} />
+						</div>
+	<div className='about-skill'>
+							<p>Python</p> <hr style={{ width: '55%' }} />
 						</div>
 					</div>
 				</div>
