346d74d6ffd0dfa43f1976a19f23f2e175f24a34
Esraa <esraaeltehawy@gmail.com>
2025-02-23 11:27:40 +0200

    add some animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { motion } from 'framer-motion';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Esraa from './components/esraa/Esraa';
@@ -8,17 +9,43 @@ import Navbar from './components/navbar/NavBar';
 import ScrollToTopButton from './components/scrollbutton/ScrollToTopButton';
 import Services from './components/services/Services';
 
+const AnimatedSection = ({ children }) => (
+	<motion.div
+		initial={{ opacity: 0, y: 50 }}
+		whileInView={{ opacity: 1, y: 0 }}
+		transition={{ duration: 0.8, ease: 'easeInOut' }}
+		viewport={{ once: false, amount: 0.3 }}>
+		{children}
+	</motion.div>
+);
+
 function App() {
 	return (
 		<div className='App'>
 			<div className='nv'>
 				<Navbar />
 			</div>
-			<Esraa />
-			<About />
-			<Services />
-			<MyWorks />
-			<Contact />
+
+			<AnimatedSection>
+				<Esraa />
+			</AnimatedSection>
+
+			<AnimatedSection>
+				<About />
+			</AnimatedSection>
+
+			<AnimatedSection>
+				<Services />
+			</AnimatedSection>
+
+			<AnimatedSection>
+				<MyWorks />
+			</AnimatedSection>
+
+			<AnimatedSection>
+				<Contact />
+			</AnimatedSection>
+
 			<Footer />
 			<ScrollToTopButton />
 		</div>
